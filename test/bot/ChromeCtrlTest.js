e179acae8808f5c5238d766a54029034e0f977e4
Valtena <ethanvaltena@gmail.com>
2018-05-13 19:19:09 +0200

    feat(chrome) : list MP

--- test/bot/ChromeCtrlTest.js
+++ test/bot/ChromeCtrlTest.js
@@ -4,31 +4,31 @@ const ChromeCtrl = require( "../../lib/bot/ChromeCtrl" );
 const expect = require( "chai" ).expect;
 const config = require( "../configtest.json" );
 
-// describe( "ChromeCtrl", function () {
-//   it( "connect to forum enter in ", (done) => {
-//      this.timeout(60000)
-//      new ChromeCtrl( config).then( ctrl => {
-//        console.log(ctrl)
-//       ctrl.login( page => {
-//         console.log("page")
-//         return page.waitForSelector('#logout').catch( err => err).then(testerDiv =>{
-//           console.log("testerDiv")
-//           expect(testerDiv).to.exist;
-//           expect(page.url()).to.equal(config.forum.link );
-//           done();
-//         })
-//       })
-//     });
-//   } );
-// } );
-
 describe( "ChromeCtrl", function () {
-  it( "connect to forum enter in ", async () => {
-     this.timeout(60000)
-    const ctrl = await new ChromeCtrl( config);
+  xit( "connect to forum enter in ", async() => {
+    this.timeout( 60000 )
+    const ctrl = await new ChromeCtrl( config );
     const page = await ctrl.login();
-    const testerDiv = await page.waitForSelector('#logout').catch( err => err)
-    expect(testerDiv).to.exist;
-    expect(page.url()).to.equal(config.forum.link );
+    const testerDiv = await page.waitForSelector( '#logout' ).catch( err => err )
+    expect( testerDiv ).to.exist;
+    expect( page.url() ).to.equal( config.forum.link );
   } );
+
+
+  describe( 'test need to be connected' , function () {
+
+    let promiseConnectedCtrl;
+    before( function () {
+      promiseConnectedCtrl = new ChromeCtrl( config ).then( ctrl => {
+        return ctrl.login().then( page => ctrl );
+      } )
+    } );
+
+    it( 'list MP', async() => {
+      const list = await promiseConnectedCtrl.then( ctrl => ctrl.listMP());
+      expect( list ).to.deep.equal( [ {
+        name: "test mp"
+      } ] )
+    } )
+  })
 } );
