1cfe699e613a89871c82fac6d184e0970a994b36
Valtena <ethanvaltena@gmail.com>
2018-05-15 00:30:03 +0200

    feat(chrome):send mp works

--- test/bot/ChromeCtrlTest.js
+++ test/bot/ChromeCtrlTest.js
@@ -5,7 +5,7 @@ const expect = require( "chai" ).expect;
 const config = require( "../configtest.json" );
 
 describe( "ChromeCtrl", function () {
-  xit( "connect to forum enter in ", async() => {
+  it( "connect to forum enter in ", async() => {
     this.timeout( 60000 )
     const ctrl = await new ChromeCtrl( config );
     const page = await ctrl.login();
@@ -15,7 +15,7 @@ describe( "ChromeCtrl", function () {
   } );
 
 
-  describe( 'test need to be connected' , function () {
+  describe( 'test need to be connected', function () {
 
     let promiseConnectedCtrl;
     before( function () {
@@ -25,10 +25,22 @@ describe( "ChromeCtrl", function () {
     } );
 
     it( 'list MP', async() => {
-      const list = await promiseConnectedCtrl.then( ctrl => ctrl.listMP());
+      const list = await promiseConnectedCtrl.then( ctrl => ctrl.listMP() );
       expect( list ).to.deep.equal( [ {
         name: "test mp"
       } ] )
     } )
-  })
+
+    it( 'send MP', async() => {
+      const list1 = await promiseConnectedCtrl.then( ctrl => ctrl.listMP() );
+      await promiseConnectedCtrl.then( ctrl => ctrl.sendMP( config.name, "sendMP", "ceci est un test" ) )
+      const list2 = await promiseConnectedCtrl.then( ctrl => ctrl.listMP() )
+      expect( list1.length + 1 ).to.equal( list2.length )
+    } )
+
+    it( 'send MP to an inexistant user must fail', async() => {
+        return promiseConnectedCtrl.then( ctrl => ctrl.sendMP( config.name, "sendMPMustFail", "ceci est un test" ) )
+        .then(() => expect("no error throw").to.equal(null)).catch( err => expect(err).to.exist);
+    } )
+  } )
 } );
