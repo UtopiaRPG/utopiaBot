88ccfc404482a1d95cefdd16e7cec888438ba600
Valtena <ethanvaltena@gmail.com>
2018-05-20 10:54:45 +0200

    feat(chrome) : test bot

--- test/mockCommand.js
+++ test/mockCommand.js
@@ -3,8 +3,10 @@ const Command = require("../lib/commands/command");
 class MockCommand extends Command {
 
   constructor(bot){
-    this.bot = this.bot;
-    this.bot.commands["mock"] = function(message, fragments){
+    console.log("bot", bot)
+    super();
+    bot.commands["mock"] = function(message, fragments){
+      console.log("reply")
         message.reply(JSON.stringify({message: message, fragments:fragments}))
     }
   }
--- test/unit/bot.test.js
+++ test/unit/bot.test.js
@@ -3,25 +3,42 @@ const Bot = require( "../../lib/bot" );
 const expect = require( "chai" ).expect;
 const EventEmitter = require('events').EventEmitter;
 const sinon = require('sinon');
+const lang = require("../../lib/lang/fr-fr.json")
 
 
 const config = {
-  modules:['../../test/mockModule'],
+  modules:['../../test/mockCommand'],
+  lang: "fr-fr"
 }
 
 
 describe( "bot", function () {
 
-  it( "unkown command reply", async() => {
+  it( "mock command reply", () => {
     const client = new EventEmitter();
     const replySpy = sinon.spy(function(message){})
-    emitter.on('foo', spy);
-    const message = sinon.mock({
-      content: "!UB unkown test command",
+    const message = {
+      content: "!UB mock test command",
       reply: replySpy
-    });
+    };
     const bot = new Bot(config,client);
     client.emit("message", message);
-    sinon.assert.calledWith(spy, 'bar', 'baz')
+    sinon.assert.called(replySpy);
+    expect('{"message":{"content":"!UB mock test command"},"fragments":["!UB","mock","test","command"]}'
+    ).to.deep.equals(replySpy.getCall(0).args[0]);
+    // sinon.assert.calledWith(replySpy, 'bar', 'baz')
+  } );
+
+  it( "unkown command reply", () => {
+    const client = new EventEmitter();
+    const replySpy = sinon.spy(function(message){})
+    const message = {
+      content: "!UB unkown test command",
+      reply: replySpy
+    };
+    const bot = new Bot(config,client);
+    client.emit("message", message);
+    sinon.assert.called(replySpy);
+    expect(lang.unknowCommand).to.deep.equals(replySpy.getCall(0).args[0]);
   } );
 })
