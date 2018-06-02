48f65725032af8e34ca94d3c8dbec486b2e258c5
Kao <floconetflanelle@gmail.com>
2018-06-02 16:18:52 +0200

    Development (#9)
    * Correcting an issue caused by a wrong "working directory" in PhpStorm"
    
    * Fixing a little typo.
    
    * Finishing the translation of some strings.
    
    * Removing debug outputs.
    
    Upgrading main.js -> autoReconnect no more exists as it's now a default behavior of the lib.
    Promise needs to be handled. Right now node.js is just warning, but in a close future the code will crash if there's no catch for promises.

--- main.js
+++ main.js
@@ -2,7 +2,9 @@ const bot=require("./lib/bot.js");
 const Discord = require('discord.js');
 var config= require("./config.json");
 
-const discord = new Discord.Client({autoReconnect:true});
-discord.login(config.token);
+const discord = new Discord.Client();
+discord.login(config.token).catch(function (error) {
+    console.log(error);
+});
 
 new bot(config, discord);
