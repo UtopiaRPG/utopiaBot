5576bb72037f78b8e2148368103fbf8c502b8dc8
Valtena <ethanvaltena@gmail.com>
2018-12-16 22:43:15 +0100

    fix(core): basique function works, need register method

--- main.js
+++ main.js
@@ -3,8 +3,10 @@ const Discord = require('discord.js');
 var config= require("./config.json");
 
 const discord = new Discord.Client();
+new bot(config, discord);
+
+console.log("try connect");
 discord.login(config.token).catch(function (error) {
-    console.log(error);
+    console.log(error || "connected");
 });
 
-new bot(config, discord);
